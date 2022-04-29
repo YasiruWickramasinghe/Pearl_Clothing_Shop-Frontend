de10f0d1370498125bb1dc20a69af4eb8780b2f0
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-04-29 19:01:28 +0530

    React Router Implimented

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Cart />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
