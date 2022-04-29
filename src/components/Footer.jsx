1a0cc34b09ba73b2f1341c876cb2c3db97735ec3
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-04-29 12:49:46 +0530

    Home page Completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,137 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.h1`
+  display: flex;
+`;
+
+const SocialIcon = styled.h1`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50px;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>LAMA.</Logo>
+        <Desc>
+          There are many variations of passages of lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected human,
+          or randomised words which don't look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="385999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4485F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Women Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> 622 Dixie path , South
+          tobington 98451
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +1 235 45 45
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> contact@pearl.lk
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 import Navbar from "./../components/Navbar";
@@ -13,6 +15,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
